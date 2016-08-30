fbf5e7d0c216f162f57f7cebf072a022101dfb0b
Grzegorz Mrozik <grzegorzm@medius-group.com>
2016-08-30 09:32:26 +0200

    It is possible to choose role while registrating new user

--- src/Mrozik.Nebraska/Controllers/AccountController.cs
+++ src/Mrozik.Nebraska/Controllers/AccountController.cs
@@ -17,17 +17,20 @@ namespace Mrozik.Nebraska.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<ApplicationRole> _roleManager;
         private readonly ILogger _logger;
 
         public AccountController(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             IEmailSender emailSender,
-            ILoggerFactory loggerFactory)
+            ILoggerFactory loggerFactory,
+            RoleManager<ApplicationRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _emailSender = emailSender;
+            _roleManager = roleManager;
             _logger = loggerFactory.CreateLogger<AccountController>();
         }
 
@@ -86,7 +89,9 @@ namespace Mrozik.Nebraska.Controllers
         public IActionResult Register(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
-            return View();
+
+            var availableRoles = _roleManager.Roles.Select(r => r.Name).ToList();
+            return View(new RegisterViewModel() { AvailableRoleNames = availableRoles });
         }
 
         //
@@ -103,14 +108,19 @@ namespace Mrozik.Nebraska.Controllers
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
-                    // Send an email with this link
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
-                    //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
-                    //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
-                    _logger.LogInformation(3, "User created a new account with password.");
-                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                    result = await _userManager.AddToRoleAsync(user, model.RoleName);
+                    if (result.Succeeded)
+                    {
+                        // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
+                        // Send an email with this link
+                        //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                        //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+                        //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
+                        //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
+                        _logger.LogInformation(3, "User created a new account with password.");
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+                    AddErrors(result);
                 }
                 AddErrors(result);
             }
--- src/Mrozik.Nebraska/Models/AccountViewModels/RegisterViewModel.cs
+++ src/Mrozik.Nebraska/Models/AccountViewModels/RegisterViewModel.cs
@@ -16,6 +16,13 @@ namespace Mrozik.Nebraska.Models.AccountViewModels
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+
+        public IList<string> AvailableRoleNames { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public string RoleName { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
         [DataType(DataType.Password)]
@@ -27,4 +34,5 @@ namespace Mrozik.Nebraska.Models.AccountViewModels
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
+
 }
