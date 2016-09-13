ad6096e6b3a63f673631db746644296221503557
Grzegorz Mrozik <grzegorz@mrozik.com.pl>
2016-09-13 20:23:32 +0200

    Simple filters added to grid

--- src/Mrozik.Nebraska/DataTablesModel/Parameters.cs
+++ src/Mrozik.Nebraska/DataTablesModel/Parameters.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Mrozik.Nebraska.Models;
 
 namespace Mrozik.Nebraska.DataTablesModel
@@ -10,8 +11,33 @@ namespace Mrozik.Nebraska.DataTablesModel
         public int Start { get; set; }
         public int Length { get; set; }
 
-        public Search  /*Dictionary<string, string>*/ Search { get; set; }
         public IList<ColumnOrder/*Dictionary<string, string>*/> Order { get; set; }
         public IList<Column/*Dictionary<string, string>*/> Columns { get; set; }
+
+        public Filters Filters { get; set; }
+    }
+
+    public class Filters
+    {
+        public string Search { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ScansAvailability ScansAvailability { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public string DepartmentName { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        All,
+        InProgress,
+        Done
+    }
+
+    public enum ScansAvailability
+    {
+        All,
+        WithScans,
+        WithoutScans
     }
 }
